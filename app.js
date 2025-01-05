3cc3fd3b0d72b18d0a6d749c522db6661ef23f09
Denis <denis.shagim@icloud.com>
2025-01-05 15:05:31 +0500

    feat: ad users in list

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
-// Event logic
+// Globals
+const todoList = document.getElementById("todo-list");
+const userSelect = document.getElementById('user-todo')
+let todos = [];
+let users = [];
+// Attach Events
 
 document.addEventListener("DOMContentLoaded", initApp);
 
-// Async logic
+// Basic Logic
+function getUserName(userId) {
+  const user = users.find((u) => u.id === userId);
+  return user.name;
+}
+function printTodo({ id, userId, title, completed }) {
+  const li = document.createElement("li");
+  li.className = "todo-item";
+  li.dataset.id = id;
+  li.innerHTML = `<span>${title} <i>by</i> <b>${getUserName(
+    userId
+  )}</b></span>`;
+
+  const status = document.createElement("input");
+  status.type = "checkbox";
+  status.checked = completed;
+
+  const close = document.createElement("span");
+  close.innerHTML = "&times;";
+  close.className = "close";
+
+  li.prepend(status);
+  li.append(close);
+  todoList.prepend(li);
+}
+
+function createUserOption(user) {
+  const option = document.createElement("option");
+  option.value = user.id;
+  option.innerText = user.name;
+
+  userSelect.append(option)
+}
+// Event Logic
+function initApp() {
+  Promise.all([getAllTodos(), getAllUsers()]).then((values) => {
+    [todos, users] = values;
+    todos.forEach((todo) => printTodo(todo));
+    users.forEach((user) => createUserOption(user));
+  });
+}
+// Async Logic
 
 async function getAllTodos() {
   const response = await fetch("https://jsonplaceholder.typicode.com/todos");
